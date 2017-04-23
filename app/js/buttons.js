a19b7b90918491833e7ba2eedd566932aff963fa
Peter Schafhalter <pschafhalter@berkeley.edu>
2017-04-23 11:52:03 -0700

    Got rid of remove icon in sidebar due to display issues

--- app/js/buttons.js
+++ app/js/buttons.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     $('.tag-cloud').on('click', '#pill', function () {
-        $(this).toggleClass('pill-faded');   
+        $(this).toggleClass('pill-faded');
     });
 
     $('.tag-cloud').on('mouseenter', '#pill', function () {
         $(this).addClass('pill-hover');
-        $(this).find('#remove').removeClass('pill-faded');  
-        $(this).find('#remove').removeClass('hideRemoveBtn');  
-        $(this).find('#remove').addClass('showRemoveBtn');  
+        $(this).find('#remove').removeClass('pill-faded');
+      //  $(this).find('#remove').removeClass('hideRemoveBtn');
+      //  $(this).find('#remove').addClass('showRemoveBtn');
     });
 
     $('.tag-cloud').on('mouseleave', '#pill', function () {
-        $(this).removeClass('pill-hover');   
-        $(this).find('#remove').removeClass('showRemoveBtn');  
-        $(this).find('#remove').addClass('hideRemoveBtn');  
+        $(this).removeClass('pill-hover');
+        //$(this).find('#remove').removeClass('showRemoveBtn');
+        //$(this).find('#remove').addClass('hideRemoveBtn');
     });
 });
